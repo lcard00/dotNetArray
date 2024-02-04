a2a56eed03ebe92bb50c6216d6a82c3b886f090d
Leonardo Cardoso <lcard00@outlook.com>
2024-02-04 18:52:56 -0300

    arrays

--- Program.cs
+++ Program.cs
@@ -4,7 +4,39 @@ array[0] = 72;
 array[1] = 64;
 array[2] = 50;
 
-foreach (int item in array)
+// foreach (var item in array)
+// {
+//     Console.WriteLine(item);
+// }
+
+
+Array.Resize(ref array, array.Length + 1);
+
+array[3] = 55;
+array[3] = 69;
+
+// foreach (var item in array)
+// {
+//     Console.WriteLine(item);
+// }
+
+List<string> stringList = [];
+
+stringList.Add("SP");
+stringList.Add("BA");
+stringList.Add("MG");
+stringList.Add("RJ");
+stringList.Add("SC");
+
+
+Console.WriteLine($"Quantidade de estados rergistrados {stringList.Count}\n");
+
+foreach (var item in stringList)
 {
     Console.WriteLine(item);
+}
+
+if (stringList.Contains("MG"))
+{
+    Console.WriteLine("\nUai sô!!");
 }
--- obj/Debug/net8.0/dotNetBasic.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotNetBasic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotNetBasic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f687b7884894c2641ded7e8d0d43e3f83b4b3191")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotNetBasic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotNetBasic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
